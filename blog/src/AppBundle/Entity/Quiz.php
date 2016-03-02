3ee94c72087730dcf79b91003ab2a8abf0ef8501
Jean-Philippe BLOND <jean-philippeblond@MacBook-Air-de-Jean-Philippe.local>
2016-03-02 10:41:46 +0100

    Quiz > PartieJP > Entity & Repository pour User, Quizz, Question

---
+++ blog/src/AppBundle/Entity/Quiz.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Quiz
+ *
+ * @ORM\Table(name="quiz")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\QuizRepository")
+ */
+class Quiz
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=70)
+     */
+    private $nom;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     * @return Quiz
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+}
