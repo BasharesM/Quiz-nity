743d913aa56e42acca26258e4f7c6e16760ac425
root <root@contact@michelbashares.fr>
2016-03-03 18:06:38 +0100

    Gestion de la pagination mise dans un repository

--- myphoto/src/AppBundle/Controller/QuizToChooseController.php
+++ myphoto/src/AppBundle/Controller/QuizToChooseController.php
@@ -5,7 +5,6 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class QuizToChooseController extends Controller {
 
@@ -13,19 +12,17 @@ class QuizToChooseController extends Controller {
      * @Route("/quiz-to-choose", name="quiz-to-choose")
      */
     public function quizToChooseAction(Request $request) {
-        $em    = $this->get('doctrine.orm.entity_manager');
-        $dql   = "SELECT a FROM AppBundle:Quiz a";
-        $query = $em->createQuery($dql);
+        $query = $this->getDoctrine()->getRepository('AppBundle:Quiz')->findQueryForAll();
 
-        $paginator  = $this->get('knp_paginator');
+        $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
-            $query, /* query NOT result */
-            $request->query->getInt('page', 1)/*page number*/,
-            1/*limit per page*/
+                $query, /* query NOT result */
+                $request->query->getInt('page', 1), /* page number */
+                2 /* limit per page */
         );
 
-    // parameters to template
-    return $this->render('AppBundle:default:quizToChoose.html.twig',array('pagination' => $pagination));
-}
+        // parameters to template
+        return $this->render('AppBundle:default:quizToChoose.html.twig', array('pagination' => $pagination));
+    }
 
 }
