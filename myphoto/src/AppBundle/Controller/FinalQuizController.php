21f59b7cf988bfd521a67cc0360d114706d8a154
TemiC <temidayoifekoya@yahoo.fr>
2016-03-03 12:59:47 +0100

    Ajout de la page de fin de quiz

---
+++ myphoto/src/AppBundle/Controller/FinalQuizController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class FinalQuizController extends Controller {
+
+    /**
+     * @Route("/quiz-end", name="quiz-end")
+     */
+    public function quizEndAction(Request $request) {
+        $url = "url";
+        $content = $this->render('AppBundle:default:quizEnd.html.twig', array('url' => $url));
+        return new Response($content);
+    }
+
+}
