78eca19d9d0ea52b09d321e21d280cdd0cf7799d
TemiC <temidayoifekoya@yahoo.fr>
2016-03-03 11:49:58 +0100

    Ajout de la pagination pour les quiz

--- myphoto/src/AppBundle/Controller/QuizToChooseController.php
+++ myphoto/src/AppBundle/Controller/QuizToChooseController.php
@@ -13,12 +13,19 @@ class QuizToChooseController extends Controller {
      * @Route("/quiz-to-choose", name="quiz-to-choose")
      */
     public function quizToChooseAction(Request $request) {
-        $content = $this->render('AppBundle:default:quizToChoose.html.twig', array(
-            'quizs' => $this->getDoctrine()->getRepository('AppBundle:Quiz')->findAll()
-                )
+        $em    = $this->get('doctrine.orm.entity_manager');
+        $dql   = "SELECT a FROM AppBundle:Quiz a";
+        $query = $em->createQuery($dql);
+
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            1/*limit per page*/
         );
 
-        return new Response($content);
-    }
+    // parameters to template
+    return $this->render('AppBundle:default:quizToChoose.html.twig',array('pagination' => $pagination));
+}
 
 }
